3b167a3532272e4ac160d1bb7bf2c9e8614dcbf0
YIHONG <akashidaiki2@gmail.com>
2023-10-27 12:07:55 +0800

    完成 BMI 計算和頁面間傳值練習

---
+++ calc.php
@@ -0,0 +1,19 @@
+<?php
+// $_GET = []； $_GET 的預設值
+if (!empty($_GET)) {
+
+
+  $w = !empty($_GET['weight']) ? $_GET['weight'] : '沒有體重資料';
+  $h = !empty($_GET['height']) ? $_GET['height'] : '沒有身高資料';
+
+  $bmi = round($w / $h * $h, 2);
+  // echo "體重:$w";
+  // echo ", ";
+  // echo "身高:$h";
+  // echo ", ";
+  // echo "BMI:$bmi";
+
+  header("location:BMI.php?w=$w&h=$h&bmi=$bmi");
+} else {
+  header("location:BMI.php?m=請輸入必要訊息");
+}
